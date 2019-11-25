f523117723343246adf73b3ec13a6c437e935d0b
keanuplayz <keanutimmermans8@gmail.com>
2019-11-25 20:34:19 +0100

    Rewrote userinfo command.

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,34 +1,25 @@
-const Discord = module.require('discord.js');
-const moment = require('moment');
+const Discord = require('discord.js');
+const colours = require("../colours.json");
+const moment = require('moment')
 
 module.exports.run = async (client, message, args) => {
-    const joinDiscord = moment(user.createdAt).format('llll');
-    const joinServer = moment(user.joinedAt).format('llll');
-    let member = message.mentions.members.first() || message.member,
-  user = member.user;
-    let embed = new Discord.RichEmbed()
-        .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
-        .setDescription(`${user}`)
-        .setColor(`RANDOM`)
-        .setThumbnail(`${user.displayAvatarURL}`)
-        .addField('Joined at:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-        .addField('Status:', user.presence.status, true)
-        .addField('Roles:', member.roles.map(r => `${r}`).join(' | '), true)
-        .setFooter(`ID: ${user.id}`)
-        .setTimestamp();
+    
+    var userinfo = new Discord.RichEmbed()
+        .setAuthor(user.avatarURL, user.tag)
+        .setColor("red_light")
+        .addField("Username:", user.username)
+        .addField("Status:", user.presence.status)
+        .addField("Joined at:", moment(message.guild.member.get(user.id).joinedAt).format("MMMM Do YYYY, h:mm a"))
+        .addField("Registered at:", moment(user.createdAt).format("MMMM Do YYYY, h:mm a"))
+    message.channel.send(userInfo);
 
-    if (e) {
-        message.react('❌')
-    } else{
-        message.channel.send({ embed: embed });
-        return; 
-    }
 }
 
 module.exports.config = {
     name: "userinfo",
+    noaliases: "No aliases",
     aliases: ["ui"],
     usage: ".userinfo",
-    description: "Displays info about a user.",
+    description: "Shows you info about a user.",
     accessibleby: "Members"
 }
