24d02f9a54a32ae2b972ea0107012efa3c168bff
Keanu <keanutimmermans8@gmail.com>
2020-03-17 11:34:21 +0100

    Added VC rename command

---
+++ commands/desc.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+exports.run = (client, message, args, level) => {
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) return message.channel.send("You are not in a voice channel.");
+    if (!args[0]) return message.channel.send("Please provide the current voice channel name as the first argument.");
+    if (!args[1]) return message.channel.send("Please provide the new voice channel name as the second argument.");
+    message.guild.channels.find("name", args[0]).setName(args[1]);
+};
+exports.conf = {
+   enabled: true,
+   guildOnly: false,
+   aliases: [],
+   permLevel: "User"
+};
+exports.help = {
+   name: "desc",
+   category: "Utility",
+   description: "Renames current voice channel.",
+   usage: "desc [current name] [name]"
+};
