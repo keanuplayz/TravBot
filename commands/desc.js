86d44223521c42cfdd3b627c9672c42a6eb5ccf7
Keanu <keanutimmermans8@gmail.com>
2020-03-17 11:46:04 +0100

    Added old channel name to alert.

--- commands/desc.js
+++ commands/desc.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args, level) => {
     if (!voiceChannel) return message.channel.send("You are not in a voice channel.");
     if (!args[0]) return message.channel.send("Please provide the current voice channel name as the first argument.");
     if (!args[1]) return message.channel.send("Please provide the new voice channel name as the second argument.");
-    message.guild.channels.find("name", args[0]).setName(args[1]).then(message.channel.send("Changed channel name to " + args[1]));
+    message.guild.channels.find("name", args[0]).setName(args[1]).then(message.channel.send("Changed channel name from " + args[0] + " " + "to " + args[1] + "."));
 };
 exports.conf = {
    enabled: true,
