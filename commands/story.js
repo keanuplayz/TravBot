8493720ed3ff80e39d45426d4caa52e3ac05814c
Keanu <keanutimmermans8@gmail.com>
2020-04-01 15:07:51 +0200

    Expanded on story command.

--- commands/story.js
+++ commands/story.js
@@ -1,5 +1,13 @@
 /* eslint-disable no-unused-vars */
+const Discord = require("discord.js");
+const fs = require("fs");
 exports.run = async (client, message, args, level) => {
+    const stories = JSON.parse(fs.readFileSync(__dirname + "/storage/stories.json", "utf8"));
+    const sender = message.author;
+    if (!stories[sender.id + message.guild.id]) stories[sender.id + message.guild.id] = {};
+    if (!stories[sender.id + message.guild.id].title) stories[sender.id + message.guild.id].title = "";
+    if (!stories[sender.id + message.guild.id].description) stories[sender.id + message.guild.id].description = "";
+
     if (!args[0]) return message.channel.send("Please specify a story name.");
 
     if (args[0] == "list") {
@@ -8,29 +16,42 @@ exports.run = async (client, message, args, level) => {
             "description": "This embed shows all current stories.\n(Extracted from `stories.json`)",
             "color": 10901330,
             "footer": {
-                "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+                "icon_url": message.guild.iconURL,
                 "text": "TravBot Story Service"
             },
             "thumbnail": {
-                "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                "url": client.user.displayAvatarURL
             },
             "author": {
                 "name": "TravBot Story Service | List",
                 "url": "https://discordapp.com",
-                "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                "icon_url": client.user.displayAvatarURL
             },
             "fields": [{
                 "name": "stories.name",
                 "value": "stories.description"
             }]
         };
-        channel.send({
+        message.channel.send({
             embed
         });
     }
 
-    const stories = fs.readFileSync(__dirname + "/storage/stories.json");
-    fs.appendFile(__dirname + "/storage/stories.json", ("\n" + args[0]));
+    if (args[0] == "create") {
+        stories[sender.id + message.guild.id].title = args[1];
+        stories[sender.id + message.guild.id].description = args[2];
+        message.channel.send(args[2].join(" "), {
+            embed: {
+                title: "Story created!",
+                description: `Title: **${args[1]}.**\nDescription: **${args[2]}.**\nYour story has been tied to your user account. Ask a moderator for help from this point on.`,
+                color: 0xe004f
+            }
+        });
+    }
+
+    fs.writeFile(__dirname + "/storage/stories.json", JSON.stringify(stories), err => {
+        if (err) console.log(err);
+    });
 };
 
 exports.conf = {
