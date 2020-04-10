43b58511e00caa2f55f0fed7b8359b73f87a74b2
BuildTools <carlos.bullon99@gmail.com>
2020-04-10 15:58:50 -0500

    yes.

--- commands/leave.js
+++ commands/leave.js
@@ -3,7 +3,6 @@ exports.run = async (client, message, args, level) => {
   let fetched = client.active.get(message.guild.id) || undefined
   if (!fetched) return message.channel.send("There currently isn't any music playing in this guild!")
   if (message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send("Sorry, you aren't connected to the same channel.")
-  //if (!fetched.dispatcher.paused) return message.channel.send("The music isn't paused.")
   const vc = message.guild.me.voiceChannel;
   if (vc) vc.leave();
   client.active.delete(message.guild.id)
