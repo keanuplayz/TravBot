04a164732046129ebf1024d7199d24ef12eb16c2
TravBot <hondzee69@gmail.com>
2020-04-11 15:54:50 +0000

    Travis beautify build 374

--- commands/search.js
+++ commands/search.js
@@ -1,37 +1,37 @@
 const search = require("yt-search");
 
 exports.run = async (client, message, args) => {
-  search(args.join(" "), function(err, res) {
-    if (err) return message.channel.send("Sorry, something went wrong.");
-    const videos = res.videos.slice(0, 10);
-    let resp = "";
-    for (var i in videos) {
-      resp += `**[${parseInt(i)+1}]:** \`${videos[i].title}\`\n`;
-    }
-    resp += `\nChoose a number between \`1-${videos.length}\``;
-    let results;
-    message.channel.send(resp).then(a => results = a);
-    const filter = m => !isNaN(m.content) && m.content < videos.length + 1 && m.content > 0 && message.author.id == m.author.id;
-    const collector = message.channel.createMessageCollector(filter);
-    collector.videos = videos;
-    collector.once("collect", function(m) {
-      results.delete();
-      m.delete();
-      const commandFile = client.commands.get("play");
-      commandFile.run(client, message, [this.videos[parseInt(m.content) - 1].url]);
+    search(args.join(" "), function(err, res) {
+        if (err) return message.channel.send("Sorry, something went wrong.");
+        const videos = res.videos.slice(0, 10);
+        let resp = "";
+        for (var i in videos) {
+            resp += `**[${parseInt(i)+1}]:** \`${videos[i].title}\`\n`;
+        }
+        resp += `\nChoose a number between \`1-${videos.length}\``;
+        let results;
+        message.channel.send(resp).then(a => results = a);
+        const filter = m => !isNaN(m.content) && m.content < videos.length + 1 && m.content > 0 && message.author.id == m.author.id;
+        const collector = message.channel.createMessageCollector(filter);
+        collector.videos = videos;
+        collector.once("collect", function(m) {
+            results.delete();
+            m.delete();
+            const commandFile = client.commands.get("play");
+            commandFile.run(client, message, [this.videos[parseInt(m.content) - 1].url]);
+        });
     });
-  });
 };
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: "User"
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "User"
 };
 exports.help = {
-  name: "search",
-  category: "Music",
-  description: "Search for a video on YouTube.",
-  usage: "search [song name]"
+    name: "search",
+    category: "Music",
+    description: "Search for a video on YouTube.",
+    usage: "search [song name]"
 };
