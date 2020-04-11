6f0d2d281077188fcedf2c29a7dcbbea540a735c
Keanu <keanutimmermans8@gmail.com>
2020-04-11 15:23:04 +0200

    Updated whoami's keys.

--- commands/whoami.js
+++ commands/whoami.js
@@ -8,7 +8,8 @@ exports.run = async (client, message, args, level) => {
         "229636002443034624": "The ***God*** of being Smug.",
         "280876114153308161": "The best girl.",
         "175823837835821067": "The somehow sentient pear.",
-        "173917366504259585": "The blueberry with horns.",
+        "145839753118351360": "The blueberry with horns.",
+        "173917366504259585": "A talented developer.",
         "216112465321263105": "The red strawberry cat.",
         "394808963356688394": "The baka girl we don't deserve.",
         "142200534781132800": "The masters of chaos.",
@@ -21,7 +22,10 @@ exports.run = async (client, message, args, level) => {
         "374298111255773184": "The cutest fox around.",
         "150400803503472640": "The big huggy turtle boye.",
         "620777734427115523": "The small huggy turtle boye.",
-        "310801870048198667": "The extremely talented artist and modder."
+        "310801870048198667": "An extremely talented artist and modder.",
+        "328223274133880833": "The stealthiest hitman.",
+        "219661798742163467": "An extremely talented artist and modder.",
+        "440399719076855818": "You are, uhh, Stay Put, Soft Puppy, Es-Pee, Swift Pacemaker, Smug Poyo, and many more.\n...Seriously, this woman has too many names."
     };
     const response = obj[`${message.author.id}`];
     if (!response) return message.channel.send("You haven't been added to the registry yet!");
