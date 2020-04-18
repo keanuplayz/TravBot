616492f153b13363eb83c34908c846eaaa9d8f13
Keanu <keanutimmermans8@gmail.com>
2020-04-18 15:12:15 +0200

    Fixed desc command.

--- commands/desc.js
+++ commands/desc.js
@@ -3,9 +3,9 @@ exports.run = (client, message, args, level) => {
     const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) return message.channel.send("You are not in a voice channel.");
     if (!args) return message.channel.send("Please provide a new voice channel name.");
-    client.channels.get(voiceChannel.id)
-        .setName(args.join(" "))
-        .then(message.channel.send("Changed channel name from " + `"${voiceChannel}"` + " " + "to " + `"${args.join(" ")}"` + "."));
+    const changeVC = client.channels.get(voiceChannel.id);
+    message.channel.send("Changed channel name from " + `"${voiceChannel}"` + " " + "to " + `"${args.join(" ")}"` + ".")
+    .then(changeVC.setName(args.join(" ")));
 };
 exports.conf = {
     enabled: true,
