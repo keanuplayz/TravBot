5d67a097e3c0a72f662c9961e3a91ae8ad72d19b
Keanu <keanutimmermans8@gmail.com>
2020-04-18 15:21:44 +0200

    Implemented permission check for desc.

--- commands/desc.js
+++ commands/desc.js
@@ -2,6 +2,7 @@
 exports.run = (client, message, args, level) => {
     const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) return message.channel.send("You are not in a voice channel.");
+    if (!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send("I am lacking the required permissions to perform this action.");
     if (!args) return message.channel.send("Please provide a new voice channel name.");
     const changeVC = client.channels.get(voiceChannel.id);
     message.channel.send("Changed channel name from " + `"${voiceChannel}"` + " " + "to " + `"${args.join(" ")}"` + ".")
