79bc3573014a8b0acf4cfb4d3a56e5da0e1e99c1
watduhhekbro <watduhhekbro@gmail.com>
2020-06-19 04:04:54 -0500

    Added the ability to get other users' avatars and see emote usage

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,6 +1,29 @@
 /* eslint-disable no-unused-vars */
 exports.run = async (client, message, args, level) => {
-    message.reply(message.author.avatarURL);
+    // avatar <ping>
+    // NOTE: This MUST come before user id since /\d+/ is more generic.
+    if (args.length >= 1 && /<@.?\d+>/g.test(args[0])) {
+        let targetID = args[0].match(/\d+/g)[0];
+        client.fetchUser(targetID).then(user => {
+            message.reply(user.avatarURL);
+        });
+    }
+    // avatar <user id>
+    else if (args.length >= 1 && /\d+/g.test(args[0])) {
+        client.fetchUser(args[0]).then(user => {
+            message.reply(user.avatarURL);
+        });
+    }
+    // avatar <name with spaces>
+    else if (args.length > 0) {
+        let name = args.join(" ");
+        let user = client.users.find(user => user.username.includes(name));
+        if (user) message.reply(user.avatarURL);
+    }
+    // avatar (no arguments)
+    else {
+        message.reply(message.author.avatarURL);
+    }
 };
 exports.conf = {
     enabled: true,
@@ -11,6 +34,6 @@ exports.conf = {
 exports.help = {
     name: "avatar",
     category: "Fun",
-    description: "Replies with your avatar.",
-    usage: "avatar"
+    description: "Replies with your avatar. You can also search by a user's ID, username, or mention them.",
+    usage: "avatar [id/username/ping]"
 };
